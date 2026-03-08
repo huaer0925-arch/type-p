669c7dcc6c20521771fc5d855391ae7ecc4ef60f
kalnal0928 <kalnal0928@gmail.com>
2026-03-08 11:45:57 +0900

    first_commit

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase 설정 및 초기화
 const firebaseConfig = {
-  apiKey: "AIzaSyBFX6GniaMIkDDDgKzxUPP5S0mtu1UfA4E",
-  authDomain: "type-p-3eec8.firebaseapp.com",
-  projectId: "type-p-3eec8",
-  storageBucket: "type-p-3eec8.firebasestorage.app",
-  messagingSenderId: "29245150954",
-  appId: "1:29245150954:web:cc936070b76e828f24e993",
-  measurementId: "G-NDRDCSXMRQ"
+  apiKey: "AIzaSyBCTQl16_-duMf0aOyR5rd_QZXWhBTZYBw",
+  authDomain: "type-p-2aa36.firebaseapp.com",
+  projectId: "type-p-2aa36",
+  storageBucket: "type-p-2aa36.firebasestorage.app",
+  messagingSenderId: "137203688372",
+  appId: "1:137203688372:web:68a75b4a477807007f88e1",
+  measurementId: "G-ZFELE1JC2T"
 };
 
 // Firebase 앱 초기화
@@ -22,4 +22,4 @@ const storage = firebase.storage();
 auth.setPersistence(firebase.auth.Auth.Persistence.SESSION);
 
 // 관리자 이메일
-const ADMIN_EMAIL = 'leeshouxuan@gmail.com';
+const ADMIN_EMAIL = 'huaer0925@gmail.com';
